f16836199986780b964deba5632fce26396c626c
Daniel Havlik <dh@gocept.com>
2023-08-14 14:17:34 +0200

    re-add delay

--- megaretoot.py
+++ megaretoot.py
@@ -47,6 +47,7 @@ reblog_history = [l.strip() for l in reblog_history]
 for account in mast.account_following(mast.me(), limit=200):
     uname = account['username']
     print(f'Searching for matching toots from {uname}.')
+    time.sleep(0.2)
     uid = str(account['id'])
     uid_fn = f'{uid}.log'
     if os.path.isfile(uid_fn):
